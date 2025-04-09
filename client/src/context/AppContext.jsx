97269c0c5852ab17cdce4b7184f655d38fed28f5
rezaazimibar <www.beta04@gmail.com>
2025-04-09 16:45:27 +0330

    add full navbar to project

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null)
+    const [isSeller, setIsSeller] = useState(false)
+
+    const value = {navigate, user, setUser, setIsSeller, isSeller}
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = ()=>{
+    return useAppContext(AppContext)
+}
