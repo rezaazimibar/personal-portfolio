3e77080f723a57e7eef20428461bc2eddf9e6c72
rezaazimibar <www.beta04@gmail.com>
2025-04-27 23:16:07 +0330

    add categories section

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BestSeller() {
+  return (
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default BestSeller
