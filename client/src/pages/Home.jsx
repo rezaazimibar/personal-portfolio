43250d5cf73522ac9113e3c16cbd1812f0cf382d
rezaazimibar <www.beta04@gmail.com>
2025-05-04 21:04:19 +0330

    add the footer and bottom of the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
         <Categories/>
         <BestSeller/>
         <BottomBanner/>
+        <NewsLetter/>
     </div>
   )
 }
