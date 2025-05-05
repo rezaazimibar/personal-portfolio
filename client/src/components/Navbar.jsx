6210b065c4cffc9d276b67923cee191c5f9245eb
rezaazimibar <www.beta04@gmail.com>
2025-05-05 23:42:52 +0330

    product page was add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,18 +5,18 @@ import { AppContext, useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery } = useAppContext()
+    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery, getCartCount } = useAppContext()
 
     const logout = async () => {
         setUser(null)
         navigate('/')
     }
-    useEffect(()=>{
+    useEffect(() => {
 
-        if(searchQuery.length > 0){
+        if (searchQuery.length > 0) {
             navigate("/products")
         }
-    },[searchQuery])
+    }, [searchQuery])
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
@@ -33,13 +33,13 @@ const Navbar = () => {
 
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input onChange={(e)=> setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <img src={assets.search_icon} alt='search' className='w-4 h-4' />
                 </div>
 
                 <div onClick={() => navigate('/cart')} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? (<button onClick={() => setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
@@ -54,11 +54,18 @@ const Navbar = () => {
                     </div>
                 )}
             </div>
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={() => navigate('/cart')} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <img src={assets.menu_icon} alt="menu" />
-            </button>
+
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" >
+                    {/* Menu Icon SVG */}
+                    <img src={assets.menu_icon} alt="menu" />
+                </button>
+            </div>
 
             {/* Mobile Menu */}
             {open && (
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -56,6 +56,28 @@ export const AppContextProvider = ({ children }) => {
         setCartItems(cartData)
     }
 
+    // Get Cart Item Count
+    const getCartCount = ()=>{
+        let totalCount = 0
+        for(const item in cartItems){
+            totalCount += cartItems[item]
+        }
+        return totalCount;
+    }
+
+    // Get Cart Total Amount
+    const getCartAmount =()=>{
+        let totalAmount = 0 
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
+
     useEffect(() => {
         fetchProducts()
     }, [])
@@ -63,7 +85,7 @@ export const AppContextProvider = ({ children }) => {
     const value = {
         navigate, user, setUser, setIsSeller, isSeller,
         showUserLogin, setShowUserLogin, products, currency, addToCart, updataCartItem, 
-        removeFromCart, cartItems, searchQuery, setSearchQuery
+        removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount
     }
 
     return <AppContext.Provider value={value}>
