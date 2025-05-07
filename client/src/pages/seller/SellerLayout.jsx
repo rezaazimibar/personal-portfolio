23987086a33441c8261ae361791108fac2ba05c6
rezaazimibar <www.beta04@gmail.com>
2025-05-07 17:45:22 +0330

    add product and product-list and orders page from admin pannel was add

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 function SellerLayout() {
 
@@ -27,24 +27,25 @@ function SellerLayout() {
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1 cursor-pointer'>Logout</button>
                 </div>
             </div>
             <div className='flex'>
                 <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
                     {sidebarLinks.map((item) => (
                         <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
-                            className={({isActive}) => `flex items-center py-3 px-4 gap-3 
-                            ${isActive ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
+                            className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
+                            ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary"
                                     : "hover:bg-gray-100/90 border-white"
                                 }`
                             }
                         >
-                            {item.icon}
+                            <img src={item.icon} alt="icon" className='w-7 h-7' />
                             <p className="md:block hidden text-center">{item.name}</p>
                         </NavLink>
                     ))}
                 </div>
+                <Outlet />
             </div>
         </>
     );
