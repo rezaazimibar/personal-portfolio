782be1ffdd12d54aaece48a0fad28abae85f61f2
rezaazimibar <www.beta04@gmail.com>
2025-05-08 20:24:40 +0330

    backend section part one

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+
+
+// Register User /api/user/register
+export const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        if(!name, email, password){
+            return res.json({success: false, message: 'Missing Details'})
+        }
+
+        const existingUser = await User.findOne({email})
+
+        if(existingUser){
+            return res.json({success: false, message: 'User already exists'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+    } catch (error) {
+        
+    }
+
+}
